cfca1dd72ab746e55e9a6697a4170fb772040dde
Arman Yazdi <f41c0n.32@gmail.com>
2022-11-16 00:44:23 +0330

    git push origin my_branch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md', 'r') as f:
 
 setup(
     name='persian-names',
-    version='1.0.2',
+    version='1.0.3',
     packages=['persian_names'],
     url='https://github.com/armanyazdi/persian-names',
     license='MIT',
