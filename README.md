c805459dab37c95cf06ce8a0d3c4f79cc35094de
Arman Yazdi <f41c0n.32@gmail.com>
2022-11-15 23:29:36 +0330

    git push origin my_branch

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # persian-names
+
 A simple Python library for generating random Persian (Farsi) names.
+
+# Installation
+
+`pip install persian-names`
+
+# Usage
+
+Let's take a look at what an example test case would look like using `persian-names`.
+
+
+To generate a Persian name in English:
+
+```apache
+import persian_names as pn
+
+pn.fullname_en('male')
+pn.fullname_en('female')
+pn.fullname_en('random')
+```
+
+To generate a Persian name in Farsi:
+
+```apache
+import persian_names as pn
+
+pn.fullname_fa('male')
+pn.fullname_fa('female')
+pn.fullname_fa('random')
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('./README.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='persian-names',
+    version='1.0',
+    packages=['persian_names'],
+    url='https://github.com/armanyazdi/persian-names',
+    license='MIT',
+    author='Arman Yazdi',
+    description='A simple Python library for generating random Persian (Farsi) names.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    install_requires=['requests'],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
