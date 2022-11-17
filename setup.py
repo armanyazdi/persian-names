8ba5191d7562d8ed5b51fa621df88fd5cd1678c2
Arman Yazdi <f41c0n.32@gmail.com>
2022-11-17 22:14:16 +0330

    git push origin my_branch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md', 'r') as f:
 
 setup(
     name='persian-names',
-    version='1.1.5',
+    version='1.1.6',
     packages=['persian_names'],
     include_package_data=True,
     data_files=[('', ['persian_names/data/male.txt', 'persian_names/data/female.txt', 'persian_names/data/male_fa.txt', 'persian_names/data/female_fa.txt'])],
@@ -18,7 +18,21 @@ setup(
     keywords='persian names, farsi names, iranian names, name generator',
     classifiers=[
         'License :: OSI Approved :: MIT License',
+        'Natural Language :: Persian',
+        'Natural Language :: English',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Localization'
     ],
     project_urls={
         'Source': 'https://github.com/armanyazdi/persian-names',
