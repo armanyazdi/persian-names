c07d2d3d7bfeaa48da8de1a6f970104eac8a6faa
Arman Yazdi <f41c0n.32@gmail.com>
2023-01-17 11:04:35 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md', 'r') as f:
 
 setup(
     name='persian-names',
-    version='1.2.6',
+    version='1.2.7',
     packages=['persian_names'],
     include_package_data=True,
     data_files=[('', [
